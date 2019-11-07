3cb247e63350a78f6e1dfa26dd1519c9107b5a8c
Alex Lee <hyunho98@gmail.com>
2019-11-07 21:16:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,12 @@ end
 def batch_badge_creator(array)
   array.collect{|badges| badge_maker(badges)}
 end
+
+def assign_rooms(array)
+  output = []
+
+  array.each_with_index{|name, index|
+    output.push("Hello #{name}! You'll be assigned to room #{index}!")}
+
+  output
+end
